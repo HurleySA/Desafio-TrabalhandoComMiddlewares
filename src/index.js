129ba4e45ae1638e29fc5a95531681109094ab60
Matheus <matheus.eduardo.silva@ccc.ufcg.edu.br>
2022-02-18 01:57:57 -0300

    100% testes unitários

--- src/index.js
+++ src/index.js
@@ -10,19 +10,59 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find(user => user.username === username);
+
+  if(!user) return response.status(404).json({ error: "User not found" });
+
+  request.user = user;
+
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+  if((user.pro === false && user.todos.length < 10) || user.pro === true){
+    return next();
+  }
+  return response.status(403).json({ error: "Not possible to create a new Todo" });
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  const user = users.find(user => user.username === username);
+
+  if(!user) return response.status(404).json({ error: "User not found" });
+
+  const valid = validate(id);
+
+  if(!valid) return response.status(400).json({ error: "id not validate." });
+
+  const todo = user.todos.find(todo => todo.id === id);
+
+  if(!todo) return response.status(404).json({error: "Todo not found0"})
+
+  request.user = user;
+  request.todo = todo;
+  return next();
+
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const user = users.find(user => user.id === id);
+
+  if(!user) {
+    return response.status(404).json({ error: "User not found" });
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 app.post('/users', (request, response) => {
